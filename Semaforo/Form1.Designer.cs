e756f9f678c3e940f56fcc845a977f280c024d48
Mateus <santosmateus42@outlook.com>
2025-05-30 13:52:29 -0300

    Initial commit

---
+++ Semaforo/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Semaforo
+{
+    partial class Semáforo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Semáforo));
+            this.Tempo = new System.Windows.Forms.Timer(this.components);
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnParar = new System.Windows.Forms.Button();
+            this.pibSemaforo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pibSemaforo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tempo
+            // 
+            this.Tempo.Interval = 1000;
+            this.Tempo.Tick += new System.EventHandler(this.Tempo_Tick);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(111, 304);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(287, 39);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnParar
+            // 
+            this.btnParar.Location = new System.Drawing.Point(111, 349);
+            this.btnParar.Name = "btnParar";
+            this.btnParar.Size = new System.Drawing.Size(287, 42);
+            this.btnParar.TabIndex = 2;
+            this.btnParar.Text = "Parar";
+            this.btnParar.UseVisualStyleBackColor = true;
+            this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+            // 
+            // pibSemaforo
+            // 
+            this.pibSemaforo.Image = global::Semaforo.Properties.Resources.Erased;
+            this.pibSemaforo.InitialImage = null;
+            this.pibSemaforo.Location = new System.Drawing.Point(111, 12);
+            this.pibSemaforo.Name = "pibSemaforo";
+            this.pibSemaforo.Size = new System.Drawing.Size(287, 286);
+            this.pibSemaforo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibSemaforo.TabIndex = 0;
+            this.pibSemaforo.TabStop = false;
+            // 
+            // Semáforo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 438);
+            this.Controls.Add(this.btnParar);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.pibSemaforo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Semáforo";
+            this.Text = "Semáforo";
+            ((System.ComponentModel.ISupportInitialize)(this.pibSemaforo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pibSemaforo;
+        private System.Windows.Forms.Timer Tempo;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnParar;
+    }
+}
+
---
+++ Semaforo/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semaforo
+{
+    public partial class Semáforo : Form
+    {
+        private int estado = 1;
+        private bool semaforoLigado;
+        public Semáforo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            semaforoLigado = true;
+            estado = 1;
+            Tempo.Start();
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            pibSemaforo.Image = Properties.Resources.Erased;
+            Tempo.Stop();
+        }
+
+        private void Tempo_Tick(object sender, EventArgs e)
+        {
+            switch (estado)
+            {
+                case 1:
+                    pibSemaforo.Image = Properties.Resources.Green;
+                    Tempo.Interval = 4000; // 4 segundos para verde
+                    estado = 2;
+                    break;
+                case 2:
+                    pibSemaforo.Image = Properties.Resources.Yellow;
+                    Tempo.Interval = 4000; // 2 segundos para Yellow
+                    estado = 3;
+                    break;
+                case 3:
+                    pibSemaforo.Image = Properties.Resources.Red;
+                    Tempo.Interval = 4000; // 5 segundos para Red
+                    estado = 1;
+                    break;
+
+            }
+        }
+    }
+}
